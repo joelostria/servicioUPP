1fae7c6b03fcb524411d8858cbaf7172eb59f6dd
joelostria <jmjoelmor891@gamil.com>
2017-02-15 12:59:27 -0600

    primera version 2

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: joel
+ *
+ * Created on 15 de febrero de 2017, 12:47 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
